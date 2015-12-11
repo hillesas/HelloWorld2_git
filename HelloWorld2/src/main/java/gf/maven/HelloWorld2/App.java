4dbad7c5c7f8a0e1a4b59c358d8c3a93b2d2ecbc
Sam <sammy-hill@web.de>
2015-12-11 12:52:05 +0100

    new project2

---
+++ HelloWorld2/src/main/java/gf/maven/HelloWorld2/App.java
@@ -0,0 +1,13 @@
+package gf.maven.HelloWorld2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
